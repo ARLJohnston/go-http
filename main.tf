132aeae22a39537a3a410ed54e0d96fefe7eef8d
ARLJohnston <40921666+ARLJohnston@users.noreply.github.com>
2024-07-31 16:02:29 +0100

    feat(azure): create AZ resource group using terraform

--- main.tf
+++ main.tf
@@ -1,43 +1,57 @@
 terraform {
   required_providers {
     docker = {
-      source = "kreuzwerker/docker"
+      source  = "kreuzwerker/docker"
       version = "~> 3.0.1"
     }
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.0.2"
+    }
   }
+  required_version = ">= 1.1.0"
 }
 
-variable "username" {
-  description = "GHCR Username"
-  type = string
-  sensitive = true
+# variable "username" {
+#   description = "GHCR Username"
+#   type        = string
+#   sensitive   = true
+# }
+
+# variable "password" {
+#   description = "GHCR password (PAT)"
+#   type        = string
+#   sensitive   = true
+# }
+
+# provider "docker" {
+#   registry_auth {
+#     address  = "https://ghcr.io"
+#     username = var.username
+#     password = var.password
+#   }
+# }
+
+provider "azurerm" {
+  features {}
 }
 
-variable "password" {
-  description = "GHCR password (PAT)"
-  type = string
-  sensitive = true
-}
+# resource "docker_image" "hello" {
+#   name         = "ghcr.io/arljohnston/go-http"
+#   keep_locally = false
+# }
 
-provider "docker" {
-  registry_auth {
-    address = "https://ghcr.io"
-    username = var.username
-    password = var.password
-  }
-}
+# resource "docker_container" "hello" {
+#   image = docker_image.hello.image_id
+#   name  = "tutorial"
 
-resource "docker_image" "hello" {
-  name = "ghcr.io/arljohnston/go-http"
-  keep_locally = false
-}
+#   ports {
+#     internal = 8080
+#     external = 8080
+#   }
+# }
 
-resource "docker_container" "hello" {
-  image = docker_image.hello.image_id
-  name = "tutorial"
-
-  ports {
-    internal = 8080
-    external = 8080
-  }
+resource "azurerm_resource_group" "rg" {
+  name     = "myTFResourceGroup"
+  location = "westus2"
 }
