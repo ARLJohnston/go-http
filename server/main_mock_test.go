b65314f98b010f2285c2c7cb13edd58397464f85
ARLJohnston <40921666+ARLJohnston@users.noreply.github.com>
2024-11-26 16:30:52 +0000

    test: add mock tests

--- server/main_mock_test.go
+++ server/main_mock_test.go
@@ -11,7 +11,7 @@ import (
 	"google.golang.org/grpc/status"
 )
 
-func TestFailWhenNoDB(t *testing.T) {
+func TestCreateFailWhenNoDB(t *testing.T) {
 	data, mock, err := sqlmock.New()
 	db = data
 	if err != nil {
@@ -20,7 +20,6 @@ func TestFailWhenNoDB(t *testing.T) {
 	defer db.Close()
 
 	mock.ExpectExec("INSERT INTO album").WithArgs("Title", "Artist", 5.0, "Cover").WillReturnError(fmt.Errorf("some error"))
-	mock.ExpectRollback()
 
 	album := pb.Album{Title: "Title", Artist: "Artist", Price: 5.0, Cover: "Cover"}
 
@@ -40,6 +39,122 @@ func TestFailWhenNoDB(t *testing.T) {
 		t.Errorf("Unable to convert error to status")
 	}
 	if status.Code() != codes.Internal {
+		t.Errorf("Unexpected error returned: %v", err)
+	}
+}
+
+func TestCreateFailWhenNoIdentifier(t *testing.T) {
+	data, mock, err := sqlmock.New()
+	db = data
+	if err != nil {
+		t.Fatalf("an error '%s' was not expected when opening a stub database connection", err)
+	}
+	defer db.Close()
+
+	mock.ExpectExec("INSERT INTO album").WithArgs("Title", "Artist", 5.0, "Cover").WillReturnResult(sqlmock.NewErrorResult(fmt.Errorf("some error")))
+
+	s := &Server{}
+	ctx := context.Background()
+
+	album := pb.Album{Title: "Title", Artist: "Artist", Price: 5.0, Cover: "Cover"}
+
+	id, err := s.Create(ctx, &album)
+	if id != nil {
+		t.Errorf("Expected no id to be returned, got %d", id.Id)
+	}
+	if err == nil {
+		t.Errorf("Expected an err, got nil")
+	}
+
+	status, ok := status.FromError(err)
+	if !ok {
+		t.Errorf("Unable to convert error to status")
+	}
+	if status.Code() != codes.NotFound {
+		t.Errorf("Unexpected error returned: %v", err)
+	}
+}
+
+func TestReadFailNoDB(t *testing.T) {
+	data, mock, err := sqlmock.New()
+	db = data
+	if err != nil {
+		t.Fatalf("an error '%s' was not expected when opening a stub database connection", err)
+	}
+	defer db.Close()
+	s := &Server{}
+
+	mock.ExpectQuery("SELECT * FROM album").WillReturnError(fmt.Errorf("some error"))
+
+	var stream pb.Albums_ReadServer
+
+	err = s.Read(&pb.Nil{}, stream)
+	if err == nil {
+		t.Errorf("Expected an err, got nil")
+	}
+
+	status, ok := status.FromError(err)
+	if !ok {
+		t.Errorf("Unable to convert error to status")
+	}
+	if status.Code() != codes.NotFound {
+		t.Errorf("Unexpected error returned: %v", err)
+	}
+}
+
+func TestUpdateFailsWhenNoDB(t *testing.T) {
+	data, mock, err := sqlmock.New()
+	db = data
+	if err != nil {
+		t.Fatalf("an error '%s' was not expected when opening a stub database connection", err)
+	}
+	defer db.Close()
+
+	mock.ExpectExec("UPDATE album SET").WithArgs("Title", "Artist", 5.0, "Cover", 0).WillReturnError(fmt.Errorf("some error"))
+
+	album := pb.Album{ID: 0, Title: "Title", Artist: "Artist", Price: 5.0, Cover: "Cover"}
+
+	s := &Server{}
+	ctx := context.Background()
+
+	_, err = s.Update(ctx, &pb.UpdateRequest{OldAlbum: &album, NewAlbum: &album})
+
+	if err == nil {
+		t.Errorf("Expected an err, got nil")
+	}
+	status, ok := status.FromError(err)
+	if !ok {
+		t.Errorf("Unable to convert error to status")
+	}
+	if status.Code() != codes.Unknown {
+		t.Errorf("Unexpected error returned: %v", err)
+	}
+}
+
+func TestDelereFailsWhenNoDB(t *testing.T) {
+	data, mock, err := sqlmock.New()
+	db = data
+	if err != nil {
+		t.Fatalf("an error '%s' was not expected when opening a stub database connection", err)
+	}
+	defer db.Close()
+
+	mock.ExpectExec("DELETE FROM album WHERE").WithArgs(0).WillReturnError(fmt.Errorf("some error"))
+
+	album := pb.Album{ID: 0, Title: "Title", Artist: "Artist", Price: 5.0, Cover: "Cover"}
+
+	s := &Server{}
+	ctx := context.Background()
+
+	_, err = s.Delete(ctx, &album)
+	if err == nil {
+		t.Errorf("Expected an err, got nil")
+	}
+	status, ok := status.FromError(err)
+	if !ok {
+		t.Errorf("Unable to convert error to status")
+	}
+	if status.Code() != codes.Unknown {
 		t.Errorf("Unexpected error returned: %v", err)
 	}
 }
