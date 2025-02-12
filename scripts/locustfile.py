b9786ca93ccf5ee82f52b3b229e9cc2bf7d98454
ARLJohnston <github@arljohnston.com>
2025-02-12 18:15:31 +0000

    style: use Black to format locustfile

--- scripts/locustfile.py
+++ scripts/locustfile.py
@@ -12,21 +12,27 @@ Faker.seed(4321)
 fake = Faker()
 random.seed(8765)
 
+host_addr = "192.168.12.82"
+
 created_albums = queue.Queue()
+
 
 class APIUser(grpc_user.GrpcUser):
     """Simulation of user using a developer API to directly call gRPC methods"""
-    host = "localhost:50051"
+
+    host = host_addr + ":50051"
     stub_class = album_pb2_grpc.AlbumsStub
     offset = 6
 
     @task
     def create(self):
-        album = album_pb2.Album(id = self.offset,
-                                title  = fake.bs(),
-                                artist = fake.name(),
-                                score  = random.randint(-50,50),
-                                cover  = fake.image_url())
+        album = album_pb2.Album(
+            id=self.offset,
+            title=fake.bs(),
+            artist=fake.name(),
+            score=random.randint(-50, 50),
+            cover=fake.image_url(),
+        )
         self.stub.Create(album)
         created_albums.put(self.offset)
         self.offset += 1
@@ -39,9 +45,11 @@ class APIUser(grpc_user.GrpcUser):
         except queue.Empty:
             pass
 
+
 class NormalUser(HttpUser):
     """Simulation of user using http to access page and vote on posts"""
-    host = "http://127.0.0.1:8000"
+
+    host = "http://" + host_addr + ":3000"
     wait_time = between(1, 5)
 
     @task
@@ -54,14 +62,16 @@ class NormalUser(HttpUser):
 
     @task
     def vote(self):
-        id = random.randint(1,5) # Choose one of the pre-existing albums
-        self.client.post("/post", {random.choice(["up", "down"]):id})
+        id = random.randint(1, 5)  # Choose one of the pre-existing albums
+        self.client.post("/post", {random.choice(["up", "down"]): id})
+
 
 @events.test_stop.add_listener
 def on_test_stop(environment, **kwargs):
     """Cleanup all created albums after the test finishes."""
+
     print("Cleaning up all created albums...")
-    with grpc.insecure_channel("localhost:50051") as channel:
+    with grpc.insecure_channel(host_addr + ":50051") as channel:
         stub = album_pb2_grpc.AlbumsStub(channel)
         while not created_albums.empty():
             album_id = created_albums.get(block=False)
