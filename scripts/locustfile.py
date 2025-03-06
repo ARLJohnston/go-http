eccfe55e8846774d46dc59882747dfd7d46a94ed
ARLJohnston <github@arljohnston.com>
2025-03-06 17:45:11 +0000

    Concatenate create and delete users

--- scripts/locustfile.py
+++ scripts/locustfile.py
@@ -19,10 +19,6 @@ def _(parser):
         "--host-addr", type=str, default="localhost", help="IP address of the cluster"
     )
 
-
-created_albums = queue.Queue(maxsize=50)
-
-
 class APIUser(grpc_user.GrpcUser):
     """Simulation of user using a developer API to directly call gRPC methods"""
     stub_class = album_pb2_grpc.AlbumsStub
@@ -33,36 +29,18 @@ class APIUser(grpc_user.GrpcUser):
         return self.environment.parsed_options.host_addr + ":50051"
 
     @task
-    def create(self):
-        try:
-            album = album_pb2.Album(
-                id=self.offset,
-                title=fake.bs(),
-                artist=fake.name(),
-                score=random.randint(-50, 50),
-                cover=fake.image_url(),
-            )
-            self.stub.Create(album)
-            created_albums.put(self.offset, timeout=3)
-            self.offset += 1
+    def createDelete(self):
+        album = album_pb2.Album(
+            id=self.offset,
+            title=fake.bs(),
+            artist=fake.name(),
+            score=random.randint(-50, 50),
+            cover=fake.image_url(),
+        )
+        self.stub.Create(album)
+        self.offset += 1
 
-        except queue.Full:
-            self.stub.Delete(album_pb2.Identifier(id=self.offset))
-            return
-
-        except grpc.RpcError:
-            return
-
-    @task
-    def delete(self):
-        album = -1
-        try:
-            album = created_albums.get(timeout=3)
-            self.stub.Delete(album_pb2.Identifier(id=album))
-        except grpc.RpcError:
-            created_albums.put(album)
-        except queue.Empty:
-            return
+        self.stub.Delete(album_pb2.Identifier(id=album))
 
 
 class NormalUser(FastHttpUser):
@@ -89,21 +67,3 @@ class NormalUser(FastHttpUser):
     def vote(self):
         id = random.randint(1, 5)  # Choose one of the pre-existing albums
         self.client.post("/post", {random.choice(["up", "down"]): id})
-
-
-@events.test_stop.add_listener
-def on_test_stop(environment, **kwargs):
-    """Cleanup all created albums after the test finishes."""
-
-    print("Cleaning up all created albums...")
-    with grpc.insecure_channel(environment.parsed_options.host_addr + ":50051") as channel:
-        stub = album_pb2_grpc.AlbumsStub(channel)
-        while not created_albums.empty():
-            album_id = created_albums.get(block=False)
-            try:
-                print(f"deleting {album_id}")
-                stub.Delete(album_pb2.Identifier(id=album_id))
-                print(f"Deleted album {album_id}")
-            except:
-                print(f"Failed to delete album {album_id}")
-        print("Cleanup finished")
